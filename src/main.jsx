b11d4d7b6cce04414758abb2c1c210b3c74151e6
Pichuanth <contacto@pichuanth.com>
2025-10-09 04:14:24 -0300

    test: montar render mínimo

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
+// src/main.jsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <div style={{padding: 24, fontSize: 24}}>Hola Factor Victoria 👋</div>
 );
